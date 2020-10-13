ffd521fe286e26ca25913426b157522dc42bb422
Frederick Price <rprice@pricemail.ca>
2020-10-13 18:28:24 -0400

    Fixed sudo problems in setup_os.sh

--- setup_os.sh
+++ setup_os.sh
@@ -1,34 +1,34 @@
 #! /bin/bash
 
 # Install PPA tools
-sudo apt-get --assume-yes update && apt-get -yqq upgrade && \
-        apt-get --assume-yes install \
+sudo apt-get --assume-yes update && sudo apt-get -yqq upgrade && \
+        sudo apt-get --assume-yes install \
     software-properties-common \
     curl \
     bash \
-    && apt-get -y clean
+    && sudo apt-get -y clean
 
 # Neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable
 # Node
-sudo curl -L https://deb.nodesource.com/setup_14.x | bash -
+sudo curl -L https://deb.nodesource.com/setup_14.x | sudo bash -
 # Yarn
-sudo curl -L https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
-sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+sudo curl -L https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && \
+sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # Software I use all the time, stow is pretty important
-sudo apt-get --assume-yes update && apt-get -yqq upgrade && \
-        apt-get --assume-yes install \
+sudo apt-get --assume-yes update && sudo apt-get -yqq upgrade && \
+        sudo apt-get --assume-yes install \
     ripgrep \
     stow \
     universal-ctags \
     golang \
     locales \
-    && apt-get -y clean
+    && sudo apt-get -y clean
 
 # Python setup, including for coc-nvim
-sudo apt-get --assume-yes update && apt-get -yqq upgrade && \
-        apt-get --assume-yes install \
+sudo apt-get --assume-yes update && sudo apt-get -yqq upgrade && \
+        sudo apt-get --assume-yes install \
     python3 \
     python3-pip \
     python-is-python3 \
@@ -39,19 +39,19 @@ sudo apt-get --assume-yes update && apt-get -yqq upgrade && \
     python-black-doc \
     python3-pynvim \
     python3-venv \
-    && apt-get -y clean
+    && sudo apt-get -y clean
 
 
 # Things needed for coc-nvim
-sudo apt-get --assume-yes update && apt-get -yqq upgrade && \
-        apt-get --assume-yes install \
+sudo apt-get --assume-yes update && sudo apt-get -yqq upgrade && \
+        sudo apt-get --assume-yes install \
     neovim \
     nodejs \
     yarn \
-    && apt-get -y clean
+    && sudo apt-get -y clean
 
 # Install Git
-sudo add-apt-repository -y ppa:git-core/ppa && apt-get update && apt-get install -y git git-lfs && apt-get -y clean
+sudo add-apt-repository -y ppa:git-core/ppa && sudo apt-get update && sudo apt-get install -y git git-lfs && sudo apt-get -y clean
 
 # Neovim module setup
 sudo yarn global add neovim
