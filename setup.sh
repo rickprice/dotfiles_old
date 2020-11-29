383f2334c8e223bce43b44dc2f1a0a0509d3a72f
rickprice <rickp@activestate.com>
2020-11-29 15:36:17 -0500

    Latest changes

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ mkdir -p ~/.cache/nvim/undo
 stow --dotfiles zshrc
 stow --dotfiles bash
 #stow --dotfiles vim
-stow --dotfiles dot-local
-stow --dotfiles dot-config
+stow --dotfiles local
+stow --dotfiles config
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
@@ -16,13 +16,13 @@ sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 ### Configure ZSH things ###
 
 # ZSH Plugins with Antibody
-antibody bundle < dot-config/antibody/zsh_plugins.txt > dot-shell_snippets/zsh_plugins.sh
-chmod a+x dot-shell_snippets/zsh_plugins.sh
+antibody bundle < config/.config/antibody/zsh_plugins.txt > shell_snippets/.shell_snippets/zsh_plugins.sh
+chmod a+x shell_snippets/.shell_snippets/zsh_plugins.sh
 
 ### Configure Shell snippets ###
 
 # Have to run stow on shell_snippets after we have set them up with antibody
-stow --dotfiles dot-shell_snippets
+stow --dotfiles shell_snippets
 
 ### Configure NeoVim ###
 
@@ -43,7 +43,7 @@ vim +PlugInstall +qall
 # link the 1st as the 2nd with relative links
 
 # Prepare a vimrc file in ~/.config/nvim folder
-ln -sf ./init.vim ~/.config/nvim/init.vim
+ln -sf ~/.config/nvim/init.vim ~/.vimrc
 
 # Link the whole ~/.config/nvim folder as ~/.vim foler
 ln -sf ./.config/nvim ~/.vim
