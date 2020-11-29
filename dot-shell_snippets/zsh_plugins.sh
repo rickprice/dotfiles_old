c91e8338d7208cbe7014b5cd8c89d8ef4537d331
rickprice <rickp@activestate.com>
2020-11-29 13:08:39 -0500

    Latest changes, something doesn't seem right with nvim

--- dot-shell_snippets/zsh_plugins.sh
+++
@@ -1,8 +0,0 @@
-source /home/fprice/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
-fpath+=( /home/fprice/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
-source /home/fprice/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /home/fprice/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /home/fprice/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /home/fprice/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /home/fprice/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/fzf/fzf.plugin.zsh
-fpath+=( /home/fprice/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/fzf )
--- setup.sh
+++ setup.sh
@@ -13,34 +13,37 @@ sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 
+### Configure ZSH things ###
+
+# ZSH Plugins with Antibody
+antibody bundle < dot-config/antibody/zsh_plugins.txt > dot-shell_snippets/zsh_plugins.sh
+chmod a+x dot-shell_snippets/zsh_plugins.sh
+
+### Configure Shell snippets ###
+
+# Have to run stow on shell_snippets after we have set them up with antibody
+stow --dotfiles dot-shell_snippets
+
+### Configure NeoVim ###
+
 # Download plug.vim for NeoVim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-### Configure Vim plugins ###
+### Configure NeoVim plugins ###
 
 vim +PlugInstall +qall
 #vim +PlugClean +qall
 #vim +PlugUpdate
 
 ### Configure Vim to use Neovim configuration ###
+
 # nvim conf dir: ~/.config/nvim
 # vim conf dir: ~/.vim
 # link the 1st as the 2nd with relative links
 
 # Prepare a vimrc file in ~/.config/nvim folder
-ln -sf ./init.vim ~/.config/nvim/vimrc
+ln -sf ./init.vim ~/.config/nvim/init.vim
 
 # Link the whole ~/.config/nvim folder as ~/.vim foler
 ln -sf ./.config/nvim ~/.vim
-
-### Configure ZSH things ###
-
-# ZSH Plugins with Antibody
-antibody bundle < ~/.config/antibody/zsh_plugins.txt > shell_snippets/.shell_snippets/zsh_plugins.sh
-chmod a+x shell_snippets/.shell_snippets/zsh_plugins.sh
-
-### Configure Shell snippets ###
-
-# Have to run stow on shell_snippets after we have set them up with antibody
-stow --dotfiles shell_snippets
