94410cfeba9819f2e7c4e0bd8e9ab4849c8a10d4
rickprice <rickp@activestate.com>
2020-12-02 01:24:24 -0500

    Latest changes, trying to get setup_os.sh working

--- setup_os.sh
+++ setup_os.sh
@@ -98,7 +98,7 @@ cd /tmp/alacritty
 
 cargo build --release
 
-udo cp target/release/alacritty /usr/local/bin
+sudo cp target/release/alacritty /usr/local/bin
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
