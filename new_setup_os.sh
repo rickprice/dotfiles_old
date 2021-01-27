dfc67aca4ad311b118c770698bfcbc32fc8087ae
rickprice <rickp@activestate.com>
2021-01-27 03:24:12 -0500

    More changes to setup script - handle PPAs

--- new_setup_os.sh
+++ new_setup_os.sh
@@ -11,4 +11,6 @@ sudo apt-get --assume-yes update && sudo apt-get -yqq upgrade && \
     python-is-python3 \
     && sudo apt-get -y clean
 
+# Bootstrap Ansible
 pip3 install ansible
+pip3 install python3-apt # This is so that we can add and remove PPA's
