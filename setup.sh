35eee66c2a52e8cf91f85290728d6acb63e6c25d
rickprice <rickp@activestate.com>
2021-02-02 23:30:52 -0500

    Change things for dotfiles again

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@
 ### Setup dotfiles directories ###
 
 mkdir -p ~/.cache/nvim/undo
-stow --dotfiles local
 stow --dotfiles zshrc
-stow --dotfiles bash
+# stow --dotfiles bash
 stow --dotfiles vim
 stow --dotfiles config
+stow --dotfiles local
 stow --dotfiles tmux
 stow --dotfiles mutt
 
