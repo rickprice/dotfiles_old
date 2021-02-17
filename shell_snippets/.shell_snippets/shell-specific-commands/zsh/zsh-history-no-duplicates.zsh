dc900c8b095f7fefa05dc7ebc7123f531c99566d
Frederick Price <rprice@pricemail.ca>
2021-02-17 17:59:40 -0500

    Modifying config files for the future

--- shell_snippets/.shell_snippets/shell-specific-commands/zsh/zsh-history-no-duplicates.zsh
+++ shell_snippets/.shell_snippets/shell-specific-commands/zsh/zsh-history-no-duplicates.zsh
@@ -6,3 +6,6 @@ setopt HIST_IGNORE_SPACE
 setopt HIST_FIND_NO_DUPS
 setopt HIST_SAVE_NO_DUPS
 setopt HIST_BEEP
+export HISTFILE="$ZDOTDIR/.zhistory"    # History filepath
+export HISTSIZE=10000                   # Maximum events for internal history
+export SAVEHIST=10000                   # Maximum events in history file
